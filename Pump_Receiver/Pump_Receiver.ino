aa3a1f085304ab71d91e74f6c8fa57213ca5335f
Simon Cozens <simon@simon-cozens.org>
2018-07-09 15:56:33 +1000

    Reciver updates

--- Pump_Receiver/Pump_Receiver.ino
+++ Pump_Receiver/Pump_Receiver.ino
@@ -1,18 +1,28 @@
 #include <SPI.h>
-#include <LoRa.h>
+#include <RH_RF95.h>
 
-#define PUMP_ON_SIGNAL_PIN  8
-#define PUMP_OFF_SIGNAL_PIN 10
+#define RFM95_CS 10
+#define RFM95_RST 9
+#define RFM95_INT 2
+#define RF95_FREQ 915.0
+
+// Singleton instance of the radio driver
+RH_RF95 rf95(RFM95_CS, RFM95_INT);
+
+#define PUMP_ON_SIGNAL_PIN  7
+#define PUMP_OFF_SIGNAL_PIN 8
 #define SIREN_PIN 12
-#define RELAY_PULSE_DURATION 100
-
-#define LORA_MAX_MESSAGE 1024
-
+#define RELAY_PULSE_DURATION 1000
 // Blinky on receipt
 #define LED 13
 
+#define SIREN_DURATION 60000
+
 int pump_status = 255; // Undefined
 unsigned long last_change;
+unsigned long to_go_on_time = 0;
+unsigned long to_go_off_time = 0;
+
 
 void siren(int amount) {
   digitalWrite(SIREN_PIN, HIGH);
@@ -21,104 +31,134 @@ void siren(int amount) {
 }
 
 void pump_on () {
-  // Turn on siren for a minute
-  pump_status = 1;
-  siren(1000 * 60);
-  last_change = millis();
+  Serial.println("Stopping siren, sending pump on pulse");
+  digitalWrite(SIREN_PIN, LOW);
   digitalWrite(PUMP_ON_SIGNAL_PIN, HIGH);
   delay(RELAY_PULSE_DURATION);
   digitalWrite(PUMP_ON_SIGNAL_PIN, LOW);
+  last_change = millis();
 }
 
 void pump_off () {
+  Serial.println("Sending pump off pulse");
   pump_status = 0;
   last_change = millis();
   digitalWrite(PUMP_OFF_SIGNAL_PIN, HIGH);
   delay(RELAY_PULSE_DURATION);
   digitalWrite(PUMP_OFF_SIGNAL_PIN, LOW);
+  // Just in case we are cancelling an existing order...
+  to_go_on_time = 0;
+  digitalWrite(SIREN_PIN, LOW);
 }
 
-void setup() {
+void schedule_pump_on() {
+  Serial.print("Sounding siren, will turn on pump at ");
+  to_go_on_time = millis();
+  to_go_on_time += SIREN_DURATION;
+  Serial.println(to_go_on_time);
+  digitalWrite(SIREN_PIN, HIGH);
+  pump_status = 1;
+  last_change = millis();
+}
+
+void scheduler() {
+  if (to_go_on_time != 0 && millis() >= to_go_on_time) {
+    pump_on();
+    to_go_on_time = 0;
+  }
+  if (to_go_on_time !=0 && millis()%1000 == 0) {
+    Serial.print("Waiting. To go on is: ");
+    Serial.print(to_go_on_time);
+    Serial.print(". Time now is ");
+    Serial.println(millis());
+  }
+}
+
+void setup()
+{
   pinMode(LED, OUTPUT);
+  pinMode(RFM95_RST, OUTPUT);
   pinMode(SIREN_PIN, OUTPUT);
   pinMode(PUMP_OFF_SIGNAL_PIN, OUTPUT);
   pinMode(PUMP_ON_SIGNAL_PIN, OUTPUT);
   last_change = millis();
-  siren(100);
-  Serial.begin(9600);
+
+  digitalWrite(RFM95_RST, HIGH);
   while (!Serial);
+  Serial.begin(9600);
+  siren(100);
+  Serial.println("Pump switch");
 
-  Serial.println("Pump receiver, version 2");
+  // manual reset
+  digitalWrite(RFM95_RST, LOW);
+  delay(10);
+  digitalWrite(RFM95_RST, HIGH);
+  delay(10);
 
-  if (!LoRa.begin(915E6)) {
-    Serial.println("Starting LoRa failed!");
+  while (!rf95.init()) {
+    Serial.println("LoRa radio init failed");
     while (1);
   }
-
-  lora_send("Hello world",12);
-  // register the receive callback
-  LoRa.onReceive(onReceive);
-
-  // put the radio into receive mode
-  LoRa.receive();
-}
-
-void loop() {
-  // do nothing
-}
-
-void onReceive(int len) {
-  // received a packet
-  uint8_t buf[LORA_MAX_MESSAGE];
-  if (len > LORA_MAX_MESSAGE) len = LORA_MAX_MESSAGE;
-
-  digitalWrite(LED, HIGH);
-
-  // read packet
-  Serial.print("Received packet '");
-  for (int i = 0; i < len; i++) {
-    char m = (char)LoRa.read();
-    if (i>=4) {
-      Serial.print(m);
-      buf[i-4]=m;
-    }
+  Serial.println("LoRa radio init OK!");
+  // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM
+  if (!rf95.setFrequency(RF95_FREQ)) {
+    Serial.println("setFrequency failed");
+    while (1);
   }
-  // print RSSI of packet
-  Serial.print("' with RSSI ");
-  Serial.println(LoRa.packetRssi());
-  delay(210);
+  Serial.print("Set Freq to: "); Serial.println(RF95_FREQ);
+  rf95.setTxPower(23, false);
+}
 
-  if (strncmp("WVFPQSTAT",(char*)buf,9) == 0) {
-        uint8_t data[] = " ";
-        Serial.print("Status query, returning: ");
+void loop()
+{
+  if (rf95.available())
+  {
+    // Should be a message for us now   
+    uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
+    uint8_t len = sizeof(buf);
+    
+    if (rf95.recv(buf, &len))
+    {
+      digitalWrite(LED, HIGH);
+      RH_RF95::printBuffer("Received: ", buf, len);
+      buf[len]=0;
+      Serial.print("Got: "); Serial.println((char*)buf);
+      Serial.print("RSSI: "); Serial.println(rf95.lastRssi(), DEC);
+      delay(210);
+      if (strncmp("WVFPQSTAT",(char*)buf,9) == 0) {
+        Serial.println("Status query");
+        uint8_t data[] = "X";
         if (millis() - last_change > 10 * 60 * 1000) {
           pump_status = 255;
         }
         data[0] = pump_status;
-        Serial.println(pump_status);
-        lora_send(data, sizeof(data));
+        rf95.send(data, 1);
+        rf95.waitPacketSent();
+        RH_RF95::printBuffer("Status query response sent: ", data, 1);
         digitalWrite(LED, LOW);
-//  } else if (strncmp("WVFPONXXX",(char*)buf,9) == 0) {
-//        uint8_t data[] = "Turning on";
-//        lora_send(data, sizeof(data));
-//        digitalWrite(LED, LOW);
-//        pump_on();
-//  } else if (strncmp("WVFPOFFXX",(char*)buf,9) == 0) {
-//    uint8_t data[] = "Turning off";
-//    lora_send(data, sizeof(data));
-//    digitalWrite(LED, LOW);
-//    pump_off();
-  } else {
-    Serial.println("Unknown packet");
+      } else if (strncmp("WVFPONXXX",(char*)buf,9) == 0) {
+        Serial.println("Turning on");
+        uint8_t data[] = "Turning on";
+        delay(200);
+        rf95.send(data, sizeof(data));
+        rf95.waitPacketSent();
+        
+        digitalWrite(LED, LOW);
+        schedule_pump_on();
+      } else if (strncmp("WVFPOFFXX",(char*)buf,9) == 0) {
+        Serial.println("Turning off");
+        uint8_t data[] = "Turning off";
+        delay(200);
+        rf95.send(data, sizeof(data));
+        rf95.waitPacketSent();
+        digitalWrite(LED, LOW);
+        pump_off();
+      }
+    }
+    else
+    {
+      Serial.println("Receive failed");
+    }
   }
+  scheduler();
 }
-
-void lora_send(char* buff, int len) {
-  LoRa.beginPacket(false);
-  LoRa.write(buff, len);
-  LoRa.endPacket();
-  Serial.println("Sent!");
-  // Go back to listening.
-  LoRa.receive();
-}
-
--- README.md
+++ README.md
@@ -9,5 +9,31 @@ Uses:
 
 Follow instructions here: https://www.loratas.io/blog/2017/11/30/workshop
 
+# Pump remote receiver
 
+Uses:
 
+* Arduino
+* Arduino LoRa Shield
+* Relay shield
+* Loud buzzer
+
+Connect all the shields together, and plug positive of buzzer into pin 12. Plug negative into GND. Connect relay number 2 to the pump on switch and relay number 3 to the pump off switch. Power via 5V DC adaptor plug.
+
+The receiver listens for commands (unencrypted, but hey - don't think anyone's going to be spoofing them) from the farm server over the LoRa radio.
+
+* On receipt of `WVFPONXXX`, it sends a signal back to the server saying `Turning on`, and then schedules the turn-on job. This consists of turning on the siren for `SIREN_DURATION` (=60000 milliseconds); after that, it will send a pulse of `RELAY_PULSE_DURATION` (=1000 milliseconds) to relay number 2.
+* On receipt of `WVFPOFFXX`, it sends back `Turning off` and then immediately sends a pulse of `RELAY_PULSE_DURATION` to relay number 3.
+* On receipt of `WVFPQSTAT`, it sends back one byte of information: 0 for "pump has recently been turned off"; 1 for "pump has recently been turned on"; 255 for "nothing has recently happened". This allows for the farm server to verify receipt of the signal it sent.
+
+If you need to adjust the relay signal duration, then:
+
+* Grab the [Arduino IDE](https://www.arduino.cc/en/Main/Software).
+* Grab a USB B - USB A lead and plug the Arduino into your PC.
+* Set Tools > Board to "Arduino/Genuino Uno"
+* Set Tools > Port to the Arduino USB port
+* You need to download and install the [Radiohead library](http://www.airspayce.com/mikem/arduino/RadioHead/).
+* Open `Pump_Receiver.ino` in Arduino and edit the `RELAY_PULSE_DURATION`.
+* Hit Upload
+
+You can debug the system by plugging the lead in and using the serial monitor in the Arduino IDE (under the tools menu).
